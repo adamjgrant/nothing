e52cc411e028814d2bbda18683069395223e6403
AG <a@b.com>
2024-12-20 19:58:24 -0800

    Passed

--- _sys/extensions/inactive/nlp.rb
+++ _sys/extensions/inactive/nlp.rb
@@ -23,16 +23,31 @@ directories_to_process.each do |current_dir|
     # Skip directories, only process files
     next unless File.file?(file_path)
 
-    # Check if the filename starts with "today" or "tomorrow" (case-insensitive)
-    if filename =~ /^(today|tomorrow)\.(.+)$/i
+    # Check if the filename starts with "today", "tomorrow", or matches "<number><unit>" format (case-insensitive)
+    if filename =~ /^(today|tomorrow|(\d+)([dwmy]))\.(.+)$/i
       prefix = $1.downcase
-      rest_of_filename = $2
+      number = $2 ? $2.to_i : nil
+      unit = $3
+      rest_of_filename = $4
 
       # Determine the appropriate date
-      date = if prefix == 'today'
+      date = case prefix
+             when 'today'
                Date.today
-             elsif prefix == 'tomorrow'
+             when 'tomorrow'
                Date.today + 1
+             else
+               # Calculate based on <number><unit>
+               case unit
+               when 'd'
+                 Date.today + number
+               when 'w'
+                 Date.today + (number * 7)
+               when 'm'
+                 Date.today >> number # Add months
+               when 'y'
+                 Date.today >> (number * 12) # Add years
+               end
              end
 
       # Construct the new filename with the date
@@ -41,7 +56,6 @@ directories_to_process.each do |current_dir|
 
       # Rename the file
       File.rename(file_path, new_file_path)
-      puts "Renamed #{filename} to #{new_filename}" # Debugging output
     end
   end
 end
