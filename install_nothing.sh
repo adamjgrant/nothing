bf4aa62cd55cc9d97920c4ec25570013a23bdb26
AG <a@b.com>
2024-12-26 05:58:28 -0800

    Improving crontab setup

--- install_nothing.sh
+++ install_nothing.sh
@@ -122,9 +122,7 @@ echo "
                                                            
                                                            "
 # Advising the user about crontab delay
-echo "Note: The cron job runs every minute. Please allow up to a minute for the first execution.
-During this time, you may not see any changes. The installation script will disappear and setup
-will complete with the first execution of NOTHING.
+echo "Note: The cron job runs every minute. Please allow up to a minute for the first execution. During this time, you may not see any changes. The installation script will disappear and setup will complete with the first execution of NOTHING.
 
 If you encounter any issues, try running this cron manually to detect any errors:
 
